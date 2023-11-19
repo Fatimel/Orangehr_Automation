e2e617af7d1a18b9c9df3bebe598e03e906feab6
Fatimel <peche06@gmail.com>
2023-11-18 21:59:33 -0500

    firtst

---
+++ src/main/java/com/Generic/Login.java
@@ -0,0 +1,14 @@
+package com.Generic;
+
+public class Login {
+	public static void main(String[] args) {
+	
+	 String name = "Fatima";
+	  double salary = 9500.75;
+	int Age = 15;
+	 
+	System.out.println( "My name is " + name);
+	System.out.println( "My salary is " + salary);
+}	
+	
+}
